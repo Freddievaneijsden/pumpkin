5acfc59030066109635a85498e26aa73526c49dd
freddievaneijsden <freddievaneijsden@gmail.com>
2024-10-15 09:30:48 +0200

    Creating javaFX project

---
+++ src/main/java/com/example/pumpkin/HelloApplication.java
@@ -0,0 +1,32 @@
+package com.example.pumpkin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        Button button = new Button("Hello World");
+        root.getChildren().add(button);
+        Scene scene = new Scene(root, 320, 240); //fxmlLoader
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
